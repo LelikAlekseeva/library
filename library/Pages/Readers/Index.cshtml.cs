12612f92b5cd3a836edfb43bc2d12a5f3390be20
idekferewf <example@gmail.com>
2026-03-31 09:30:58 +0300

    Накосячила Index.cshtml

---
+++ library/Pages/Readers/Index.cshtml.cs
@@ -0,0 +1,22 @@
+using library.Data;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace library.Pages.Readers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Readers> Students { get; set; }
+
+        public void OnGet()
+        {
+            Students = _context.Readers.ToList();
+        }
+    }
+}
